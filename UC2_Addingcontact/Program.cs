0a1c0f39cf1c4b0e1478581c7a300e313e0b02e0
ovi2410 <guneshwarifegade@gmail.com>
2022-05-07 10:35:09 +0530

    adding contact in book

---
+++ UC2_Addingcontact/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace AddressBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Welcome To Address Book System!");
+            AddrBook.GetCustomer();
+            AddrBook.ListingPeople();
+
+        }
+    }
+}
