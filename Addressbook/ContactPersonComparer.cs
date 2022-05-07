be79d4e84143b418d02f2c4041eb4c52cefa0931
ovi2410 <guneshwarifegade@gmail.com>
2022-05-07 10:26:25 +0530

    address book

---
+++ Addressbook/ContactPersonComparer.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace AddressBooks
+{
+    internal class ContactPersonComparer
+    {
+        public ContactPersonComparer()
+        {
+        }
+    }
+}
